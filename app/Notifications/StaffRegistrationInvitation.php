974a5f8d5c1d5f2c31757d1cc68f8f8fc0f134bf
kenzo0111 <147122275+kenzo0111@users.noreply.github.com>
2026-08-18 01:36:47 +0800

    Dispatch staff registration invitation synchronously

--- app/Notifications/StaffRegistrationInvitation.php
+++ app/Notifications/StaffRegistrationInvitation.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class StaffRegistrationInvitation extends Notification implements ShouldQueue
+class StaffRegistrationInvitation extends Notification
 {
     use Queueable;
 
