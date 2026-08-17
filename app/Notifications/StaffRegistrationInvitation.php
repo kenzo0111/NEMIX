732bb1d24b37f71cea24f71370df35efa9724278
kenzo0111 <147122275+kenzo0111@users.noreply.github.com>
2026-08-18 02:19:39 +0800

    Update registration URL to point directly to Register page

--- app/Notifications/StaffRegistrationInvitation.php
+++ app/Notifications/StaffRegistrationInvitation.php
@@ -30,7 +30,7 @@ class StaffRegistrationInvitation extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-        $registrationUrl = rtrim(config('app.url'), '/') . route('register.invitation', [
+        $registrationUrl = rtrim(config('app.url'), '/') . route('register', [
             'token' => $this->token,
             'email' => $notifiable->getEmailForPasswordReset(),
         ], false);
